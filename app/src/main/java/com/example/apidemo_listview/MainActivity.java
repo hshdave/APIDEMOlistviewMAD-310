ad8e60e9e80a8bf8638eb7d0ab1f06726923f0e7
hdave <hshdave2011>
2019-06-27 16:30:01 -0400

    Adapter Asyc Class

---
+++ app/src/main/java/com/example/apidemo_listview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.apidemo_listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String ls = getResources().getString(R.string.link);
+
+        try {
+            String mysts = new Asycdata().execute(ls).get();
+
+            System.out.println("This is from MainActivity :"+mysts);
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/apidemo_listview/Products.java
@@ -0,0 +1,26 @@
+package com.example.apidemo_listview;
+
+public class Products {
+    String pimg,pname;
+
+    public Products(String pimg, String pname) {
+        this.pimg = pimg;
+        this.pname = pname;
+    }
+
+    public String getPimg() {
+        return pimg;
+    }
+
+    public void setPimg(String pimg) {
+        this.pimg = pimg;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+}
